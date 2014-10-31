c0844d89d8ffcf992acc25a555c1d7ab22282320
Kristine Planche <kristineplanche@gmail.com>
2014-10-31 15:11:03 -0400

    about text and headings

--- page-full.php
+++ page-full.php
@@ -12,7 +12,8 @@ get_header();  ?>
     <?php // Start the loop ?>
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-      <div class="keyline"><h2><?php the_title(); ?></h2></div>
+      	<h2><?php the_title(); ?></h2>
+
       <?php the_content(); ?>
 
     <?php endwhile; // end the loop?>
